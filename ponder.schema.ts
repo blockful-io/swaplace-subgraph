033cfc3de7446eab21f2ee5fa2fbc42282cd90bf
0xneves <asguilift@hotmail.com>
2024-03-08 05:08:30 +0700

    Initial Commit

---
+++ ponder.schema.ts
@@ -0,0 +1,17 @@
+import { createSchema } from "@ponder/core";
+
+export default createSchema((p) => ({
+  Status: p.createEnum(["CREATED", "ACCEPTED", "CANCELED"]),
+  Database: p.createTable({
+    id: p.hex(),
+    status: p.enum("Status"),
+    allowed: p.hex(),
+    owner: p.hex(),
+    expiry: p.bigint(),
+    bid: p.string(),
+    ask: p.string(),
+    swapId: p.bigint(),
+    blockTimestamp: p.bigint(),
+    transactionHash: p.hex(),
+  }),
+}));
