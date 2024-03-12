d9405258082c8f28504902403f68f4cdc8211fe2
Rafael Sanches <rafaeldss12@gmail.com>
2024-03-12 12:00:51 -0300

    fix: test wtih Database.upsert to check if updated events are correclty fetched

--- src/Swaplace.ts
+++ src/Swaplace.ts
@@ -60,13 +60,62 @@ ponder.on("Swaplace:SwapCreated", async ({ event, context }) => {
 });
 
 ponder.on("Swaplace:SwapCanceled", async ({ event, context }) => {
-
   const { Database } = context.db;
-  const { swapId } = event.args;
+  const { swapId, owner } = event.args;
+  const { client } = context;
+  const { Swaplace } = context.contracts;
 
-  await Database.update({
+  const contractResponse = await client.readContract({
+    abi: Swaplace.abi,
+    address: Swaplace.address,
+    functionName: "getSwap",
+    args: [event.args.swapId],
+  });
+
+  let config = contractResponse.config;
+
+  const expiry: bigint = BigInt(config) & ((BigInt(1) << BigInt(96)) - BigInt(1));
+  const allowed: any = BigInt(config) >> BigInt(96);
+  config = (allowed << BigInt(96)) | BigInt(expiry);
+
+  interface Asset {
+    addr: string;
+    amountOrId: string;
+  }
+
+  let biding = contractResponse.biding.map((config) => {
+    let asset: Asset = {
+      addr: config.addr,
+      amountOrId: config.amountOrId.toString(),
+    };
+    return asset;
+  });
+
+  let asking = contractResponse.biding.map((config) => {
+    let asset: Asset = {
+      addr: config.addr,
+      amountOrId: config.amountOrId.toString(),
+    };
+    return asset;
+  });
+
+  let strinfiedBid = JSON.stringify(biding);
+  let strinfiedAsk = JSON.stringify(asking);
+
+  await Database.upsert({
      id: `0x${swapId}`,
-    data: {
+     create: {
+      swapId: swapId,
+      status: "created",
+      owner: owner,
+      allowed: allowed,
+      expiry: expiry,
+      bid: strinfiedBid,
+      ask: strinfiedAsk,
+      blockTimestamp: event.block.timestamp,
+      transactionHash: event.transaction.hash,
+     },
+     update: {
       status: "canceled",
     
     },
